707a3817dc262fe0a0f64f6a8c9c18d85970f2d6
mdcruz <marie.cruz1@news.co.uk>
2020-06-13 19:03:30 +0100

    integrate mochawesome report

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,2 +1,12 @@
 import './commands';
 import 'cypress-axe';
+import addContext from 'mochawesome/addContext';
+
+Cypress.on('test:after:run', (test, runnable) => {
+  if (test.state === 'failed') {
+    const screenshot = `${Cypress.config('screenshotsFolder')}/${
+      Cypress.spec.name
+    }/${runnable.parent.title} -- ${test.title} (failed).png`;
+    addContext({ test }, screenshot);
+  }
+});
